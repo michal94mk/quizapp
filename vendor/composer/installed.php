bf0265b96d45a4fb2ef1636a71f54205ffbb24a9
robertocarlos235 <robertocarlos23@o2.pl>
2024-07-21 00:23:00 +0200

    #1 Instaling composer and adding folders for Controllers, Models, Views in src

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'michal94mk/quizapp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9a32d3b60a27a11d4acae8a7df4d52c9cb015c80',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'michal94mk/quizapp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9a32d3b60a27a11d4acae8a7df4d52c9cb015c80',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
