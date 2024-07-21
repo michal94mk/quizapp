8884ad5a939aff519bcc9f6d65aa771a7bfc6d1b
robertocarlos235 <robertocarlos23@o2.pl>
2024-07-21 02:35:26 +0200

    #3 Add .gitignore with patterns for files and directories to ignore and env.example file with example enviroments variables

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit033e67759b863db92a470502173d32e9
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit033e67759b863db92a470502173d32e9', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit033e67759b863db92a470502173d32e9', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit033e67759b863db92a470502173d32e9
         call_user_func(\Composer\Autoload\ComposerStaticInit033e67759b863db92a470502173d32e9::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit033e67759b863db92a470502173d32e9::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
