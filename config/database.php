6df3d42463b958cf96b091c9624a19f859f623d1
robertocarlos235 <robertocarlos23@o2.pl>
2024-07-21 03:47:59 +0200

    #5 Update config/database.php with database settings.

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => getenv('DB_HOST'),
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'charset' => getenv('DB_CHARSET'),
+];
